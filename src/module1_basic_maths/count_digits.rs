c47d9df5fb3da99aab6aa94852ca111fd31a4cf2
Aditya-1207 <108715826+Aditya-1207@users.noreply.github.com>
2024-06-16 11:18:54 +0530

    count digit better

--- src/module1_basic_maths/count_digits.rs
+++ src/module1_basic_maths/count_digits.rs
@@ -1,21 +1,30 @@
-pub fn count_digits(num:i32) -> i32 {
+pub fn count_digits(num:i32) -> usize {
 
     //1. Brute force
-    let mut count = 0;
-    let mut number = num.abs();
+    // let mut count = 0;
+    // let mut number = num.abs();
+    // while number > 0 {
+    //     number /= 10;
+    //     count += 1;
+    // }
+    // return count;
 
-    while number > 0 {
-        number /= 10;
+    //2.Slightly Better
+    if num == 0 {
+        return 1
+    }
+    let mut  count = 1;
+    let mut n = num.abs();
+    while n > 10 {
+        n /= 10;
         count += 1;
     }
-
-    return count;
-
+    count
 }
 
 pub fn main()  {
     //Input
-    let number = 12345;
+    let number = -2345;
     let count = count_digits(number);
     println!("The number {} has {:?} digits.", number, count);
 }
