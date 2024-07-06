ed1ffc26505018bc851d74b4b860ec6e54200543
Aditya Kulkarni <108715826+Aditya-1207@users.noreply.github.com>
2024-07-06 16:23:22 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 //mod module1_basic_maths;
 
-mod module5_Arrays_Medium;
+mod module5_arrays_medium;
 fn main() {
     //My first Rust program :)
     //println!("Hello, world!");
     // crate::module1_basic_maths::count_digits::main();
-    //crate::module5_Arrays_Medium::two_Sum::main();
+    //crate::module5_arrays_medium::two_Sum::main();
     crate::module5_Arrays_Medium::sort012Array::main();
 
 }
